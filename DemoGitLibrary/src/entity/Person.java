2170dae4218dde658fae371c8f1c353a8601baf3
junbin-tan <junbin.tan@u.nus.edu>
2022-10-19 21:10:25 +0800

    Intital commit

---
+++ DemoGitLibrary/src/entity/Person.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Jun Bin
+ */
+@Entity
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long personId;
+    @Column(length = 32)
+    private String firstName;
+
+    public Person() {
+    }
+
+    public Person(Long personId, String firstName) {
+        this.personId = personId;
+        this.firstName = firstName;
+    }
+
+    
+    
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (personId != null ? personId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the personId fields are not set
+        if (!(object instanceof Person)) {
+            return false;
+        }
+        Person other = (Person) object;
+        if ((this.personId == null && other.personId != null) || (this.personId != null && !this.personId.equals(other.personId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Person[ id=" + personId + " ]";
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+}
